74a2e9b8fe92df57fdeae78f9686e275c8d7bd6f
Francesco Ceruti <ceppofrancy@gmail.com>
2018-06-12 15:05:01 +0200

    change deployment to a custom repository hosted in github-pages

--- dist/Flatpak/build.sh
+++ dist/Flatpak/build.sh
@@ -6,19 +6,22 @@ BRANCH="$1"
 
 set -xe
 
-# Create and add a local repository
-ostree init --mode=archive-z2 --repo=repository
-flatpak remote-add --user --no-gpg-verify --if-not-exists repo repository
+# Prepare repository
+[[ -d repo ]] || ostree init --mode=archive-z2 --repo=repo
+[[ -d repo/refs/remotes ]] || mkdir -p repo/refs/remotes && touch repo/refs/remotes/.gitkeep
+# flatpak remote-add --user --no-gpg-verify --if-not-exists repo repo
 
 # Install runtime and sdk
-flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install --user flathub org.gnome.Platform//3.28 org.gnome.Sdk//3.28
+flatpak --user remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak --user install flathub org.gnome.Platform//3.28 org.gnome.Sdk//3.28
 
-# Run the build
-flatpak-builder --verbose --force-clean --ccache --repo=repo build com.github.FrancescoCeruti.LinuxShowPlayer.json
+# Build
+flatpak-builder --verbose --force-clean --ccache --require-changes --repo=repo \
+                --subject="Nightly build of LinuxShowPlayer, `date`" \
+                build com.github.FrancescoCeruti.LinuxShowPlayer.json
 
-# Bundle
-mkdir -p out
-flatpak build-bundle repo out/LinuxShowPlayer.flatpak com.github.FrancescoCeruti.LinuxShowPlayer $BRANCH
+# Update the repository
+flatpak build-update-repo --prune --prune-depth=20 --generate-static-deltas repo
+echo 'gpg-verify-summary=false' >> repo/config
 
 set +xe
