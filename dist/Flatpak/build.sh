f9daaba1dff7bacb1ed70f6d4f0ecec40417de03
Francesco Ceruti <ceppofrancy@gmail.com>
2018-06-13 19:56:50 +0200

    revert to bintray uploads

--- dist/Flatpak/build.sh
+++ dist/Flatpak/build.sh
@@ -7,19 +7,18 @@ source "functions.sh"
 
 BRANCH="$1"
 
-set -xe
+set -e
 
 # Prepare the repository
 travis_fold start "flatpak_prepare"
-    [[ -d repo ]] || ostree init --mode=archive-z2 --repo=repo
-    [[ -d repo/refs/remotes ]] || mkdir -p repo/refs/remotes && touch repo/refs/remotes/.gitkeep
+    ostree init --mode=archive-z2 --repo=repo
 travis_fold end "flatpak_prepare"
 
 
 # Install runtime and sdk
 travis_fold start "flatpak_install_deps"
     travis_time_start
-        flatpak --user remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+        flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
         flatpak --user install flathub org.gnome.Platform//3.28 org.gnome.Sdk//3.28
     travis_time_finish
 travis_fold end "flatpak_install_deps"
@@ -27,18 +26,17 @@ travis_fold end "flatpak_install_deps"
 # Build
 travis_fold start "flatpak_build"
     travis_time_start
-        flatpak-builder --verbose --force-clean --ccache --require-changes --repo=repo \
-                        --subject="Nightly build of LinuxShowPlayer, `date`" \
+        flatpak-builder --verbose --force-clean --ccache --repo=repo \
                         build com.github.FrancescoCeruti.LinuxShowPlayer.json
     travis_time_finish
 travis_fold end "flatpak_build"
 
-# Update the repository
-travis_fold start "flatpak_update_repo"
+# Bundle the build
+travis_fold start "flatpak_bundle"
     travis_time_start
-        flatpak build-update-repo --prune --prune-depth=20 --generate-static-deltas repo
-        echo 'gpg-verify-summary=false' >> repo/config
+        mkdir -p out
+        flatpak build-bundle repo out/LinuxShowPlayer.flatpak com.github.FrancescoCeruti.LinuxShowPlayer $BRANCH
     travis_time_finish
 travis_fold end "flatpak_update_repo"
 
-set +xe
+set +e
