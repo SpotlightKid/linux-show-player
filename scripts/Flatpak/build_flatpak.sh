9f9afef2573cf285ea75fd276d2b5b3764d90d3c
Francesco Ceruti <ceppofrancy@gmail.com>
2019-03-29 00:56:42 +0100

    Updated building scripts

--- scripts/Flatpak/build_flatpak.sh
+++ scripts/Flatpak/build_flatpak.sh
@@ -15,7 +15,7 @@ echo -e "\n"
 cd "${0%/*}"
 
 # Load Environment variables
-source config.sh
+source functions.sh
 
 # Print relevant variables
 echo "<<< FLATPAK_RUNTIME = "$FLATPAK_RUNTIME
@@ -23,55 +23,15 @@ echo "<<< FLATPAK_PY_VERSION = "$FLATPAK_PY_VERSION
 echo "<<< FLATPAK_APP_ID = " $FLATPAK_APP_ID
 echo "<<< FLATPAK_APP_MODULE = " $FLATPAK_APP_MODULE
 
-echo -e "\n"
-echo "#################################"
-echo "#    Install runtime and sdk    #"
-echo "#################################"
-echo -e "\n"
+flatpak_install_runtime
 
-flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install --user --assumeyes $FLATPAK_RUNTIME
+init_venv
 
-echo -e "\n"
-echo "###########################"
-echo "#    Build the flatpak    #"
-echo "###########################"
-echo -e "\n"
+flatpak_build_manifest
 
-# If needed build the flatpak manifest
-if [ ! -f "$FLATPAK_APP_ID.json" ]; then
-    python3 prepare_flatpak.py
-fi
+flatpak_build
 
-# Prepare the repository
-ostree init --mode=archive-z2 --repo=repo
-# Build the flatpak
-flatpak-builder --verbose --force-clean --ccache --repo=repo build $FLATPAK_APP_ID.json
-
-echo -e "\n"
-echo "##########################"
-echo "#    Bundle the build    #"
-echo "##########################"
-echo -e "\n"
-
-mkdir -p out
-# Create the bundle (without blocking the script)
-flatpak build-bundle repo out/LinuxShowPlayer.flatpak $FLATPAK_APP_ID $BUILD_BRANCH &
-# pid of the last command
-pid=$!
-# Elapsed time
-elapsed="00:00"
-# If this script is killed, kill the 'build-bundle' process
-trap "kill $pid 2> /dev/null" EXIT
-# While the bundle is running print the elapsed time every second
-while kill -0 $pid 2> /dev/null; do
-    elapsed=$(echo -en $(ps -o etime= -p "$pid"))
-    echo -ne "[ $elapsed ] Building ...\r"
-    sleep 1
-done
-echo "Completed in $elapsed                                  "
-# Disable the trap on a normal exit.
-trap - EXIT
+flatpak_bundle
 
 echo -e "\n"
 echo "================= build_flatpak.sh ================="
---
+++ scripts/Flatpak/functions.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+source config.sh
+
+function flatpak_build_manifest() {
+    source ./venv/bin/activate
+    python3 prepare_flatpak.py
+    deactivate
+}
+
+function flatpak_install_runtime() {
+    echo -e "\n"
+    echo "#########################################"
+    echo "#    Install flatpak runtime and sdk    #"
+    echo "########################################"
+    echo -e "\n"
+
+    flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    flatpak install --user --assumeyes $FLATPAK_RUNTIME
+}
+
+function flatpak_build() {
+    echo -e "\n"
+    echo "###########################"
+    echo "#    Build the flatpak    #"
+    echo "###########################"
+    echo -e "\n"
+
+    # Prepare the repository
+    ostree init --mode=archive-z2 --repo=repo
+    # Build the flatpak
+    flatpak-builder --verbose --force-clean --ccache --repo=repo build $FLATPAK_APP_ID.json
+}
+
+function flatpak_bundle() {
+    echo -e "\n"
+    echo "###############################"
+    echo "#    Create flatpak bundle    #"
+    echo "###############################"
+    echo -e "\n"
+
+    mkdir -p out
+    # Create the bundle (without blocking the script)
+    flatpak build-bundle repo out/LinuxShowPlayer.flatpak $FLATPAK_APP_ID $BUILD_BRANCH &
+    # Print elapsed time
+    watch_process $!
+}
+
+function watch_process() {
+    elapsed="00:00"
+    # If this command is killed, kill the watched process
+    trap "kill $1 2> /dev/null" EXIT
+    # While the process is running print the elapsed time every second
+    while kill -0 $1 2> /dev/null; do
+        elapsed=$(echo -en $(ps -o etime= -p "$1"))
+        echo -ne "[ $elapsed ] Building ...\r"
+        sleep 1
+    done
+    # Print a "completed message"
+    echo "Completed in $elapsed                                  "
+    # Disable the trap on a normal exit.
+    trap - EXIT
+}
+
+function init_venv() {
+    # Create virtual-environment
+    python3 -m venv ./venv
+
+    # Install requirements
+    source ./venv/bin/activate
+    pip3 install --upgrade -r requirements.txt
+    deactivate
+}
