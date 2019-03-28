63786cb6b1d0e5bd1f588c4256c3497774a670f4
Francesco Ceruti <ceppofrancy@gmail.com>
2019-03-28 23:35:51 +0100

    Updated building scripts, test circleci

--- scripts/Flatpak/build_flatpak.sh
+++ scripts/Flatpak/build_flatpak.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Exit if something fails during the build
-set -e
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
 
 echo -e "\n\n"
 echo "================= build_flatpak.sh ================="
@@ -10,18 +14,14 @@ echo -e "\n"
 # Make sure we are in the same directory of this file
 cd "${0%/*}"
 
-export FLATPAK_RUNTIME="org.gnome.Platform//3.32 org.gnome.Sdk//3.32"
-export FLATPAK_PY_VERSION="3.7"
-export FLATPAK_PY_IGNORE_PACKAGES="setuptools six pygobject pycairo"
+# Load Environment variables
+source config.sh
 
-echo "FLATPAK_RUNTIME = "$FLATPAK_RUNTIME
-echo "FLATPAK_PY_VERSION = "$FLATPAK_PY_VERSION
-
-export FLATPAK_APP_ID="com.github.FrancescoCeruti.LinuxShowPlayer"
-export FLATPAK_APP_MODULE="linux-show-player"
-
-echo "FLATPAK_APP_ID = " $FLATPAK_APP_ID
-echo "FLATPAK_APP_MODULE = " $FLATPAK_APP_MODULE
+# Print relevant variables
+echo "<<< FLATPAK_RUNTIME = "$FLATPAK_RUNTIME
+echo "<<< FLATPAK_PY_VERSION = "$FLATPAK_PY_VERSION
+echo "<<< FLATPAK_APP_ID = " $FLATPAK_APP_ID
+echo "<<< FLATPAK_APP_MODULE = " $FLATPAK_APP_MODULE
 
 echo -e "\n"
 echo "#################################"
@@ -33,18 +33,15 @@ flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/fl
 flatpak install --user --assumeyes $FLATPAK_RUNTIME
 
 echo -e "\n"
-echo "#######################################"
-echo "#    Generate the flatpak manifest    #"
-echo "#######################################"
-echo -e "\n"
-
-python3 prepare_flatpak.py
-
-echo -e "\n"
 echo "###########################"
 echo "#    Build the flatpak    #"
 echo "###########################"
 echo -e "\n"
+
+# If needed build the flatpak manifest
+if [ ! -f "$FLATPAK_APP_ID.json" ]; then
+    python3 prepare_flatpak.py
+fi
 
 # Prepare the repository
 ostree init --mode=archive-z2 --repo=repo
@@ -79,5 +76,3 @@ trap - EXIT
 echo -e "\n"
 echo "================= build_flatpak.sh ================="
 echo -e "\n\n"
-
-set +e
---
+++ scripts/Flatpak/config.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export FLATPAK_RUNTIME="org.gnome.Platform//3.32 org.gnome.Sdk//3.32"
+export FLATPAK_PY_VERSION="3.7"
+export FLATPAK_PY_IGNORE_PACKAGES="setuptools six pygobject pycairo"
+
+export FLATPAK_APP_ID="com.github.FrancescoCeruti.LinuxShowPlayer"
+export FLATPAK_APP_MODULE="linux-show-player"
