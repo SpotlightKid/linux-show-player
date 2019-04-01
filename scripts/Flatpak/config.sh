c523692c36ede96c442b7e88714bfccc9239f008
Francesco Ceruti <ceppofrancy@gmail.com>
2019-04-01 23:11:27 +0200

    trying to build qt5/pyqt5

--- scripts/Flatpak/config.sh
+++ scripts/Flatpak/config.sh
@@ -8,7 +8,7 @@ export FLATPAK_SDK_VERSION=$FLATPAK_RUNTIME_VERSION
 export FLATPAK_INSTALL="$FLATPAK_RUNTIME//$FLATPAK_RUNTIME_VERSION $FLATPAK_SDK//$FLATPAK_SDK_VERSION"
 
 export FLATPAK_PY_VERSION="3.7"
-export FLATPAK_PY_IGNORE_PACKAGES="setuptools six pygobject pycairo"
+export FLATPAK_PY_IGNORE_PACKAGES="setuptools six pygobject pycairo pyqt5 sip"
 
 export FLATPAK_APP_ID="com.github.FrancescoCeruti.LinuxShowPlayer"
 export FLATPAK_APP_MODULE="linux-show-player"
